1d4f31ce742d8101c68279471254072e9ab7785e
lucentyr00101 <sarcosredilbertranz@gmail.com>
2020-04-23 16:22:50 +0800

    added funds deduct page

---
+++ pages/funds-management/deduct.vue
@@ -0,0 +1,47 @@
+<template lang="pug">
+  v-row
+    v-col.mx-auto(lg="6")
+      v-form(ref="form" @submit.prevent="submitForm" v-model="valid")
+        v-card(shaped)
+          v-card-text
+            p.headline Deduct Funds
+            v-text-field(label="Amount" prepend-inner-icon="mdi-currency-php" :rules="[rules.number, rules.required]" v-model="amount")
+            v-text-field(label="Date" prepend-inner-icon="mdi-calendar-month" :rules="[rules.required]" disabled :value="getDate")
+            v-btn.mt-5(type="submit" color="success" :disabled="!valid") Save
+</template>
+
+<script>
+import moment from 'moment'
+import { postFund } from '@/utils/api'
+
+export default {
+  data () {
+    return {
+      valid: true,
+      rules: {
+        number: v => !isNaN(v) || 'Amount field must be a number!',
+        required: v => !!v || 'This field is required!'
+      },
+      amount: 0
+    }
+  },
+  computed: {
+    getDate () {
+      return moment().format('MMMM DD, YYYY')
+    }
+  },
+  methods: {
+    async submitForm () {
+      if (this.$refs.form.validate()) {
+        try {
+          const formData = new FormData()
+          formData.append('amount', this.amount)
+          await this.$axios.post(postFund(), formData, this.$auth.getHeader())
+        } catch (err) {
+          console.log(err.response.data)
+        }
+      }
+    }
+  }
+}
+</script>
