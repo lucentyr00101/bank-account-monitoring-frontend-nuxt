adc6e1014eb7275f73254aa446e9341783c444b2
lucentyr00101 <sarcosredilbertranz@gmail.com>
2020-06-18 12:45:57 +0800

    added total funds

--- pages/funds-management/list.vue
+++ pages/funds-management/list.vue
@@ -1,13 +1,17 @@
 <template lang="pug">
-  v-data-table(:headers="headers" :items="logs" :items-per-page="pagination.per_page" :server-items-length="pagination.total" @update:page="fetchData" :loading="loading")
+  div
+    v-data-table(:headers="headers" :items="logs" :items-per-page="pagination.per_page" :server-items-length="pagination.total" @update:page="fetchData" :loading="loading")
+    h1 Total: {{ total }}
 </template>
 
 <script>
+import _ from 'lodash'
 import { listFunds } from '@/utils/api'
 
 export default {
   data () {
     return {
+      total: 0,
       logs: [],
       headers: [
         { text: 'ID', align: 'center', value: 'id' },
@@ -36,10 +40,14 @@ export default {
         this.pagination.total = res.data.meta.total
         this.pagination.per_page = res.data.meta.per_page
         this.pagination.current_page = res.data.meta.current_page
+        this.total = this.computeTotal(res.data.data)
       } catch (e) {
         console.log(e)
       }
       this.loading = false
+    },
+    computeTotal (data) {
+      return _.sumBy(data, 'amount').toLocaleString()
     }
   }
 }
